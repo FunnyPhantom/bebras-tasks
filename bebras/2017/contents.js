3248123673e0a8bcc743b5bcf7ad1514fdc06b37
RamtinMoslemi <ramtin4moslemi@yahoo.com>
2023-07-13 03:06:58 +0330

    add names

--- bebras/2017/contents.js
+++ bebras/2017/contents.js
@@ -14,7 +14,7 @@ standaloneAddContents({
      { code: "2017-FR-04-faulty-wire",
        title: "تخم‌های پنهان" },
      { code: "2017-FR-05-draw-shapes",
-       title: "Langage de formes" },
+       title: "زبان شکل‌ها" },
      { code: "2017-FR-06-pacman",
        title: "Fantôme" },
      { code: "2017-FR-07-graph-representation",
